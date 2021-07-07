b9bd0fc8df38fab19bfcb04fe0ca78c978b56f3d
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2021-07-07 16:49:32 -0300

    Desafio 1 javascript ninja

---
+++ desafi-01.js
@@ -0,0 +1,50 @@
+// Desafio Semana #1
+
+// Declarar uma variável chamada `myvar`, sem valor.
+let myvar;
+console.log(myvar)
+
+// Após declarada, atribua o valor 10 à variável `myvar`.
+myvar = 10;
+console.log(myvar)
+
+
+// Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
+let soma = 15 + 8;
+console.log(soma)
+
+// Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
+soma += 1;
+console.log(soma)
+
+// Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
+soma *= 3
+console.log(soma)
+
+// Qual é o valor da variável `soma` até aqui?
+console.log(soma)
+
+// Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
+let souninja = true;
+console.log(souninja)
+
+// Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
+let comida = ['arroz', 'feijao', 'ovo'];
+console.log(comida)
+
+// Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
+console.log(comida[1])
+
+// Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
+console.log(`soma === myvar ${soma===myvar}`);
+
+// Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
+console.log(`myvar <= soma ${myvar <= soma}`)
+
+// Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
+function divisao (a, b) {
+    return a/b;
+}
+
+// Invoque a função criada acima, passando os parâmetros 10 e 2.
+console.log(divisao(10, 2));
