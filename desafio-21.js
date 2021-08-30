bea02f0b94e5601080b0fa35041d3461b8ae1ff4
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2021-08-29 23:58:49 -0300

    Desafio 21 javascript ninja

---
+++ desafio-21.js
@@ -0,0 +1,41 @@
+/*
+O desafio de hoje será um pequeno projeto: um cronômetro!
+As regras para criação do cronômetro são as seguintes:
+1. Crie um arquivo index.html e adicione esse script a ele;
+2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
+Ele será o nosso cronômetro;
+3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
+4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
+cada segundo;
+5. Ao clicar em Stop, o cronômetro deve parar de contar;
+6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+
+Utilize o atributo data-js para nomear o campo e os botões. Você pode
+usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
+dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
+*/
+
+(function(window, document) {
+  let $inputCron = document.querySelector('#cron');
+  let $btnStart = document.querySelector('#start');
+  let $btnStop = document.querySelector('#stop');
+  let $btnReset = document.querySelector('#reset');
+  let idSetTimeOut;
+
+  $inputCron.value = 0;
+
+  function cron(){
+    idSetTimeOut = setTimeout(cron, 1000);
+    $inputCron.value++;
+  }
+
+  $btnStart.addEventListener('click', cron, false);
+  $btnStop.addEventListener('click',  function(){
+    clearTimeout(idSetTimeOut);
+  }, false)
+  $btnReset.addEventListener('click', function(){
+    clearTimeout(idSetTimeOut);
+    $inputCron.value = 0;
+  }, false)
+
+})(window, document)
